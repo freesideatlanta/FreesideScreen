99b9be478713f7b62d40089fa663ec7c8fae563b
Nathan Burnham <nburn42@gmail.com>
2017-04-23 14:00:21 -0400

    Added more install stuff

---
+++ install_base.sh
@@ -0,0 +1,16 @@
+# updates and stuff
+sudo apt-get update
+sudo apt-get upgrade
+
+#keep screen on and remove cursor if no movment
+sudo apt-get install unclutter
+
+#enable ssh
+sudo touch /boot/ssh
+
+#install photo reel dependencies
+sudo apt-get install python-opencv
+
+sudo mkdir /usr/local/bin
+
+sudo cp ../FreesideScreen /usr/local/bin
