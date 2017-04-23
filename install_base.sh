fcda5db4abec35bbf0226032809e53fa5ff5f1a3
Nathan Burnham <nburn42@gmail.com>
2017-04-23 16:17:33 -0400

    remove annoying thing asking us to change password

--- install_base.sh
+++ install_base.sh
@@ -11,6 +11,9 @@ sudo touch /boot/ssh
 #install photo reel dependencies
 sudo apt-get install python-opencv
 
+#move our stuff to a better spot
 sudo mkdir -p /usr/local/bin
-
 sudo cp -r ../FreesideScreen /usr/local/bin
+
+#remove annoying thing asking us to change password
+sudo rm /etc/xdg/lxsession/LXDE-pi/sshpwd.sh
