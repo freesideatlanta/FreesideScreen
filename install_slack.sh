4272910e00b4cace70df0a72fe46861f26741553
Nathan Burnham <nburn42@gmail.com>
2017-04-23 16:22:43 -0400

    Update install_slack.sh

--- install_slack.sh
+++ install_slack.sh
@@ -1,2 +1,4 @@
-sudo cp autostart_slack /etc/xdg/lxsession/LXDE/autostart
-sudo cp autostart_slack /etc/xdg/lxsession/LXDE-pi/autostart
+sudo cp autostart_slack /etc/xdg/lxsession/LXDE/autostart &
+sudo cp autostart_slack /etc/xdg/lxsession/LXDE-pi/autostart &
+sudo cp autostart_slack ~/.config/lxsession/LXDE-pi/autostart &
+sudo cp autostart_slack ~/.config/lxsession/LXDE/autostart &
