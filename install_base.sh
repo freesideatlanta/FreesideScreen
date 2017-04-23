eee6a18f193e9fa4d9d8b9937b120fac5e67f1c8
Nathan Burnham <nburn42@gmail.com>
2017-04-23 17:15:59 -0400

    added automatic yes on installs

--- install_base.sh
+++ install_base.sh
@@ -1,15 +1,15 @@
 # updates and stuff
 sudo apt-get update
-sudo apt-get upgrade
+sudo apt-get -y upgrade
 
 #keep screen on and remove cursor if no movment
-sudo apt-get install unclutter
+sudo apt-get install -y unclutter
 
 #enable ssh
 sudo touch /boot/ssh
 
 #install photo reel dependencies
-sudo apt-get install python-opencv
+sudo apt-get install -y python-opencv
 
 #move our stuff to a better spot
 sudo mkdir -p /usr/local/bin
