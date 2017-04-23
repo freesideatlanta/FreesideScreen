0e71f0f134aaef94565df93524712e398bbd6667
Nathan Burnham <nburn42@gmail.com>
2017-04-23 16:27:24 -0400

    Create install_welcome.sh

---
+++ install_welcome.sh
@@ -0,0 +1,4 @@
+sudo cp autostart_welcome /etc/xdg/lxsession/LXDE/autostart &
+sudo cp autostart_welcome /etc/xdg/lxsession/LXDE-pi/autostart &
+sudo cp autostart_welcome ~/.config/lxsession/LXDE-pi/autostart &
+sudo cp autostart_welcome ~/.config/lxsession/LXDE/autostart &
