e00aa745396c34c713af2a1db240757f9409e3e7
Nathan Burnham <nburn42@gmail.com>
2017-04-23 20:24:55 -0400

    Update install_next_meetups.sh

--- install_next_meetups.sh
+++ install_next_meetups.sh
@@ -1,1 +1,5 @@
+sudo cp autostart_screen /etc/xdg/lxsession/LXDE/autostart &
+sudo cp autostart_screen /etc/xdg/lxsession/LXDE-pi/autostart &
+sudo cp autostart_screen ~/.config/lxsession/LXDE-pi/autostart &
+sudo cp autostart_screen ~/.config/lxsession/LXDE/autostart &
 sudo apt-get install -y python-qt4
