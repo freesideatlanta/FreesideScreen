e10ba475b82b5667c2006811446e6bfdc569363b
Nathan Burnham <nburn42@gmail.com>
2017-06-24 15:22:27 -0400

    change to nexgt meetups

--- next_meetups.py
+++ next_meetups.py
@@ -8,36 +8,40 @@ from PyQt4.QtGui import QApplication
 from PyQt4.QtCore import QUrl
 import time
 
-def getEvent(url_path, key) :
-    responseString = ""
- 
-    params = {'city':city, 'key':key,'topic':topic}
-    r = requests.get(url_path, params = params)    
-    print(r.url)    
-    responseString = r.text
-    return responseString
+
+class Browser(QWebView):
+
+    def __init__(self):
+        QWebView.__init__(self)
+        self.loadFinished.connect(self._result_available)
+        self.events = []
+        self.goto_link()
+
+    def _result_available(self, ok):
+        frame = self.page().mainFrame()
+        self.show()
+        #print unicode(frame.toHtml()).encode('utf-8')
+        time.sleep(10)
+        self.goto_link()
+
+    def goto_link(self):
+        if(len(self.events)==0):
+            self.get_events()
+
+        event = self.events.pop(0)
+
+        link = event["link"]
+        print link
+        self.load(QUrl(link))
+
+    def get_events(self):
+        r = requests.get("https://api.meetup.com/Freeside-Atlanta/events", params = {'key':"d6d2d4d3718361f7b5a4f63201b7162"} )
+        print(r.url)
+        response = r.text
+        self.events = json.loads(response)[:10]
 
 app = QApplication(sys.argv)
 
-browser = QWebView()
-
-url_path = "https://api.meetup.com"         #Url to meetup API
-key_path = "api_key.txt"                    #Path to api_key.txt
-path_code = ""                              #var to store the url_path + the specific api path
-key = "d6d2d4d3718361f7b5a4f63201b7162"     #key for junk account    
-
-params = {'key':key}
-
-while True:
-    response = eg.getEvent("https://api.meetup.com/Freeside-Atlanta/events", key)
-    r = requests.get(url_path, params = params)
-    print(r.url)    
-    response = r.text
-    events = decodeJSON(response)
-    
-    for event in events:
-        browser.load(QUrl(event["link"]))
-        browser.show()
-        time.sleep(30)
+browser = Browser()
 
 app.exec_()
