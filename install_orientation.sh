b3c0e39bc835bd07135720ab94c132f9ec06f56d
Nathan Burnham <nburn42@gmail.com>
2017-06-24 19:05:29 -0400

    Added orientation

---
+++ install_orientation.sh
@@ -0,0 +1,4 @@
+sudo cp autostart_orientation /etc/xdg/lxsession/LXDE/autostart &
+sudo cp autostart_orientation /etc/xdg/lxsession/LXDE-pi/autostart &
+sudo cp autostart_orientation ~/.config/lxsession/LXDE-pi/autostart &
+sudo cp autostart_orientation ~/.config/lxsession/LXDE/autostart &
---
+++ orientation.py
@@ -0,0 +1,37 @@
+import cv2
+import urllib
+import numpy as np
+import random
+import time
+#from pydrive.auth import GoogleAuth
+#from pydrive.drive import GoogleDrive
+from os import listdir
+
+from os.path import isfile, join
+
+cv2.namedWindow("main", cv2.WND_PROP_FULLSCREEN)          
+cv2.setWindowProperty("main", cv2.WND_PROP_FULLSCREEN, cv2.cv.CV_WINDOW_FULLSCREEN)
+
+
+#gauth = GoogleAuth()
+#gauth.LocalWebserverAuth()
+
+#drive = GoogleDrive(gauth)
+
+
+# fix by authing here https://script.google.com/d/1jX5iS0ofuh8fjbDMBdGk7C6uQDa8xveN5sleiCBRgdOkUEbECxgYIupg/edit?usp=drive_web
+# on account freesideprogramming@gmail.com
+
+path = "/usr/local/bin/FreesideScreen/orientation"
+while(True):
+	try:
+		for f in listdir(path):
+			full_path = join(path, f)
+			if not isfile(full_path):
+				continue
+			img = cv2.imread(full_path, cv2.CV_LOAD_IMAGE_COLOR);
+
+			cv2.imshow("main",img)
+			if cv2.waitKey(8000) & 0xff == 27: quit()
+	except Exception as e:
+		print "rror", e
